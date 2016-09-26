74c7e79a4969e6b239294b1588c5293467685bbd
cf7 <cfranco7@lion.lmu.edu>
2016-09-26 12:36:32 -0700

    configuring nav tabs js

--- static/js/casemanager.js
+++ static/js/casemanager.js
@@ -1,8 +1,29 @@
-$(function () {
+$(function (event) {
 
     $("#addcasenote").click(function () {
         console.log("added new case note");
         window.location.href = "casenotepage.html";
     });
 
+    $(".tablinks").click(function (event) {
+        var currentTabID = $(this).attr('href');
+        $(currentTabID).show().siblings().hide();
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+        event.preventDefault();
+    });
+
+
+    // var tabcontent = $(".tab-pane");
+    // console.log(tabcontent);
+    // for (var index = 0; index < tabcontent.length; index++) {
+    //     tabcontent[index].style.display = "none";
+    // }
+
+    // var tablinks = $(".tablinks");
+    // console.log(tablinks);
+    // for (var index = 0; index < tablinks.length; index++) {
+    //     tablinks[index].class = tablinks[index].class.replace(" active", "");
+    // }
+
+    // event.currentTarget.class += " active";
 });
