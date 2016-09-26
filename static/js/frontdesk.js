e5f8d2de29f80eaa1e2883642b9121ef75e23408
Mary-Kate <mreid10@lion.lmu.edu>
2016-09-26 14:55:36 -0700

    added more options to the new Drop-in page. Modified layout'

--- static/js/frontdesk.js
+++ static/js/frontdesk.js
@@ -13,17 +13,85 @@ $(function (event) {
     });
 
 
-    // var tabcontent = $(".tab-pane");
-    // console.log(tabcontent);
-    // for (var index = 0; index < tabcontent.length; index++) {
-    //     tabcontent[index].style.display = "none";
-    // }
+// From:http://bootsnipp.com/snippets/featured/checked-list-group
+    $(function () {
+        $('.list-group.checked-list-box .list-group-item').each(function () {
+            
+            // Settings
+            var $widget = $(this),
+                $checkbox = $('<input type="checkbox" class="hidden" />'),
+                color = ($widget.data('color') ? $widget.data('color') : "primary"),
+                style = ($widget.data('style') == "button" ? "btn-" : "list-group-item-"),
+                settings = {
+                    on: {
+                        icon: 'glyphicon glyphicon-check'
+                    },
+                    off: {
+                        icon: 'glyphicon glyphicon-unchecked'
+                    }
+                };
+                
+            $widget.css('cursor', 'pointer')
+            $widget.append($checkbox);
 
-    // var tablinks = $(".tablinks");
-    // console.log(tablinks);
-    // for (var index = 0; index < tablinks.length; index++) {
-    //     tablinks[index].class = tablinks[index].class.replace(" active", "");
-    // }
+            // Event Handlers
+            $widget.on('click', function () {
+                $checkbox.prop('checked', !$checkbox.is(':checked'));
+                $checkbox.triggerHandler('change');
+                updateDisplay();
+            });
+            $checkbox.on('change', function () {
+                updateDisplay();
+            });
+              
 
-    // event.currentTarget.class += " active";
+            // Actions
+            function updateDisplay() {
+                var isChecked = $checkbox.is(':checked');
+
+                // Set the button's state
+                $widget.data('state', (isChecked) ? "on" : "off");
+
+                // Set the button's icon
+                $widget.find('.state-icon')
+                    .removeClass()
+                    .addClass('state-icon ' + settings[$widget.data('state')].icon);
+
+                // Update the button's color
+                if (isChecked) {
+                    $widget.addClass(style + color + ' active');
+                } else {
+                    $widget.removeClass(style + color + ' active');
+                }
+            }
+
+            // Initialization
+            function init() {
+                
+                if ($widget.data('checked') == true) {
+                    $checkbox.prop('checked', !$checkbox.is(':checked'));
+                }
+                
+                updateDisplay();
+
+                // Inject the icon if applicable
+                if ($widget.find('.state-icon').length == 0) {
+                    $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
+                }
+            }
+            init();
+        });
+    
+        $('#get-checked-data').on('click', function(event) {
+            event.preventDefault(); 
+            var checkedItems = {}, counter = 0;
+            $("#check-list-box li.active").each(function(idx, li) {
+                checkedItems[counter] = $(li).text();
+                counter++;
+            });
+            $('#display-json').html(JSON.stringify(checkedItems, null, '\t'));
+        });
+    });
+
+
 });
