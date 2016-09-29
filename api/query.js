87f14689af03bed78b808e4e88d3cd04b84695ec
IrakliK <ikhizani@lion.lmu.edu>
2016-09-29 09:50:03 -0700

    Introducing query connections.

--- api/query.js
+++ api/query.js
@@ -2,7 +2,10 @@ var Path = require('path');
 var Queries = require(Path.join(__dirname, 'queries.js')); // jshint ignore:line
 
 var query = {
-
+    getEvent: function(){
+        connection.query(Queries.getEvent(), function(err, rows, fields) {
+            if (err) throw err;
+    }
 };
 
 module.exports = query;
