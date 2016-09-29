be7283cfcc2870a4bd220660b9142d43a18bdcf0
IrakliK <ikhizani@lion.lmu.edu>
2016-09-29 09:54:25 -0700

    Fixed some syntax

--- api/query.js
+++ api/query.js
@@ -2,10 +2,12 @@ var Path = require('path');
 var Queries = require(Path.join(__dirname, 'queries.js')); // jshint ignore:line
 
 var query = {
-    getEvent: function(){
+    getEvent: function() {
         connection.query(Queries.getEvent(), function(err, rows, fields) {
-            if (err) throw err;
+            if (err) {
+                throw err;
+            }
     }
-};
+)};
 
 module.exports = query;
