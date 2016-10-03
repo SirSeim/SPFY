63a96fe3cebea2276da395dc4e3539efab84a7a8
cf7 <cfranco7@lion.lmu.edu>
2016-10-02 23:28:27 -0700

    fixed MySQL syntax for script

--- db_setup/db.sql
+++ db_setup/db.sql
@@ -1,5 +1,8 @@
-DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS spfy.client;
 
 
-CREATE TABLE client (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, firstName VARCHAR(45), lastName VARCHAR(45), email VARCHAR(45));
+CREATE TABLE spfy.client (id INT NOT NULL AUTO_INCREMENT, firstName VARCHAR(45), lastName VARCHAR(45), email VARCHAR(45), PRIMARY KEY(id));
+INSERT INTO spfy.client (firstName, lastName, email) VALUES ("Bob", "Stevens", "email@email.com");
+
+SELECT * FROM spfy.client;
 
