29cae3822d9211767940b472940e7dba831c331f
NAnguiano <nanguia1@lion.lmu.edu>
2016-10-07 19:06:59 -0700

    Add new data structure.

--- static/js/addnewclient.js
+++ static/js/addnewclient.js
@@ -1,29 +1,139 @@
 var IntakeForm = React.createClass({
   getInitialState: function () {
-    return {firstName: "", nickname: "", lastName: "",
-            birthMonth: "01", birthDate: "01", birthYear: "2016",
-            email: "", howHeard: ""};
+    return {firstName: "",
+            lastName: "", 
+            nickname: "", 
+            personCompletingIntake: "", 
+            intakeDate: moment().toISOString(), 
+            HMISConsent: false, 
+            firstTime: true, 
+            caseManager: "", 
+            caseManagerID: 0,
+            phoneNumber: "",
+            email: "",
+            dob: "",
+            intakeAge: 0,
+            providedID: true,
+            stateID: "CA",
+            otherID: "", 
+            reference: "",
+            referenceOther: "",
+            services: "",
+            disability: false,
+            lastGradeCompleted: "",
+            someCompleted: "",
+            currentlyAttending: [],
+            graduated: false,
+            firstLanguage: "English",
+            preferredLanguage: "English",
+            maritalStatus: "Unmarried",
+            militaryService: "",
+            healthInsurance: false,
+            gender: "Male",
+            genderIdentification: "Male",
+            preferredPronoun: "",
+            ethnicity: "White",
+            race: "",
+            lastSleepingLocation: "", 
+            lastSleepingDuration: "", 
+            firstDayFirstTimeHomeless: "", 
+            currentHomelessStartDate: "", 
+            currentHomelessLength: "", 
+            homelessEpisodeCount: "", 
+            locationBeforeWestLA: "", 
+            durationInWestLA: "", 
+            housingInstabilityCause: "", 
+            stableHousingObstacle: "", 
+            housingInterest: "", 
+            naturalConnection: "", 
+            contactName: "", 
+            contactPhoneNumber: "", 
+            contactRelationship: "", 
+            currentlyPregnant: "", 
+            firstPregnancy: "", 
+            preNatalCareReceived: "", 
+            preNatalCareLocation: "", 
+            preNatalCareDesired: "", 
+            trimester: "", 
+            babyDueDate: "", 
+            hasOtherChildren: "", 
+            dcfsOpenCase: "", 
+            childrenWithFamilyOrFriends: "", 
+            substanceAbuse: "", 
+            choiceSubstance: "", 
+            injectedDrugs: "", 
+            treatmentInterest: "", 
+            mentalServicesReceived: "", 
+            mentalServicesLocation: "", 
+            mentalMedication: "", 
+            helpAcquiringMedicine: "", 
+            internalReferral: "", 
+            externalReferral: "", 
+            income: "", 
+            birthCity: "", 
+            birthState: "", 
+            birthCountry: "", 
+            employed: "", 
+            lookingForEmployment: "", 
+            fosterCare: "", 
+            socialSecurityNumber: "", 
+            caringForAnimals: "", 
+            goodNeighborContract: "", 
+            storyPhotoVideoAudioForm: "", 
+            informationReleaseAuthorized: "", 
+            servicesConsent: "", 
+            showerInstructions: "", 
+            showerGuidelines: "", 
+            dropInGuidelines: "", 
+            intakeConfirmation: "", 
+            immediateNeedsConfirmation: "", 
+            documentsSigned: "", 
+            sleepingBag: "", 
+            backpack: ""
+            };
   },
   handleSubmit: function (e) {
-    console.log("fudge");
     e.preventDefault();
+    var data = this.state;
+    console.log(data)
     var firstName = this.state.firstName.trim();
     var nickname = this.state.nickname.trim();
     var lastName = this.state.lastName.trim();
-    var birthDate = this.state.birthDate.trim();
-    var birthday = moment(birthYear + "-" + birthMonth + "-" + birthDate);
-    birthday.toISOString();
-    if (isNaN(birthDate)) {
-      console.log("this is false.");
-      return
-    } else if (!firstName || !lastName) {
+    //var birthDate = this.state.birthDate.trim();
+    //var birthday = moment(birthYear + "-" + birthMonth + "-" + birthDate);
+    //birthday.toISOString();
+    //if (isNaN(birthDate)) {
+    //  console.log("this is false.");
+    //  return
+    if (!firstName || !lastName) {
       console.log("I was missing a critical piece.");
     } else {
-      // ajax call
+      // Ajax will go here
     };
   },
+  handleHMISConsentChange: function (e) {
+    var getVal = e.currentTarget.value,
+        boolVal = false;
+    if (getVal === "true") {
+      boolVal = true;
+    };
+    this.setState({HMISConsent: boolVal});
+  },
+  handleFirstTimeChange: function (e) {
+    var getVal = e.currentTarget.value,
+        boolVal = false;
+    if (getVal === "true") {
+      boolVal = true;
+    };
+    this.setState({firstTime: boolVal});
+  },
+  handleIntakeDateChange: function (e) {
+    console.log("Handling...");
+    var date = moment(e).toISOString();
+    this.setState({intakeDate: date});
+  },
   handleFirstNameChange: function (e) {
-    this.setState({firstName: e.target.value});
+    this.setState({firstName: e.target.value.trim()});
   },
   handleNicknameChange: function (e) {
     this.setState({nickname: e.target.value});
@@ -31,66 +141,143 @@ var IntakeForm = React.createClass({
   handleLastNameChange: function (e) {
     this.setState({lastName: e.target.value});
   },
-  handleBirthMonthChange: function (e) {
-    this.setState({birthMonth: e});
-  },
-  handleBirthDateChange: function (e) {
-    this.setState({birthDate: e});
-  },
-  handleBirthYearChange: function (e) {
-    this.setState({birthYear: e});
+  handleBirthdayChange: function (e) {
+    this.setState({dob: e.target.value}); // will not work right now
   },
   handleEmailChange: function (e) {
     this.setState({email: e.target.value});
   },
-  handleHowHeard: function (e) {
-    this.setState({howHEar: e.target.value})
+  handleProvidedIDChange: function (e) {
+    var getVal = e.currentTarget.value,
+        boolVal = false;
+    if (getVal === "true") {
+      boolVal = true;
+    };
+    this.setState({providedID: boolVal})
+  },
+  handleStateIDChange: function (e) {
+    this.setState({stateID: e.target.value});
+  },
+  handleOtherIDChange: function (e) {
+    this.setState({otherID: e.target.value});
+  },
+  handleReferenceChange: function (e) {
+    this.setState({refernce: e.target.value})
+  },
+  handleReferenceOtherChange: function (e) {
+    this.setState({referenceOther: e.target.value});
   },
   render: function () {
-    var currentYear = new Date().getFullYear();
+    var currentYear = new Date().getFullYear(),
+        todaysDate = moment();
     return (
       <div>
         <form className="intakeForm" onSubmit={this.handleSubmit}>
-          First Name: <input type="text"
-                        onChange={this.handleFirstNameChange} />
-          Nickname: <input type="text" 
-                      onChange={this.handleNicknameChange} />
-          Last Name: <input type="text"
-                      onChange={this.handleLastNameChange} />
-          <br />
-          <DateOfBirthDropdown 
-            changeMonth={this.handleBirthMonthChange}
-            changeDate={this.handleBirthDateChange} 
-            changeYear={this.handleBirthYearChange} 
-            currentYear={currentYear} />
-          Email: <input type="text"
-                  onChange={this.handleEmailChange} />
-          <br />
-          ID Provided?  {/* Make into component that asks for ID and ID type if yes. currently NOT STORED. */}
-              <input type="radio" name="idProvided" value="idYes" /> Yes
-              <input type="radio" name="idProvided" value="idNo" /> No
-          <br />
-          How did you hear about Spy? {/* Same as ID. Make other option functional */}
-          <select name="howHear" defaultValue="outreach" onChange={this.handleHowHeard}>
-            <option value="outreach">SPY Outreach</option>
-            <option value="friends">Friends</option>
-            <option value="web">Web</option>
-            <option value="Other">Other</option>
-          </select>
-          {/* Not included: Services */}
-          <br /> 
-          <input type="submit" value="Submit" />
-        </form>
-        <div className="trashDiv">
-          <p>
-            My name is {this.state.firstName} ({this.state.nickname}) {this.state.lastName}.
-          </p>
-        </div>
+            HMIS Consent: <input type="radio" name="HMIS" value="true" 
+                                  checked={this.state.HMISConsent === true}
+                                  onChange={this.handleHMISConsentChange} /> Yes
+                          <input type="radio" name="HMIS" value="false" 
+                                  checked={this.state.HMISConsent === false} 
+                                  onChange={this.handleHMISConsentChange} /> No
+            <br />
+            First Time Attending? <input type="radio" name="firstTime" value="true" 
+                                    checked={this.state.firstTime === true}
+                                    onChange={this.handleHMISConsentChange} /> Yes
+                                  <input type="radio" name="firstTime" value="false" 
+                                    checked={this.state.firstTime === false} 
+                                    onChange={this.handleHMISConsentChange} /> No
+            {/*Assigned Case Manager:  TO DO */}
+            <br />
+            {/*Date of Intake: <DateDropdown handleChange={this.handleIntakeDateChange} />*/}
+            First Name: <input type="text"
+                          onChange={this.handleFirstNameChange} />
+            Nickname: <input type="text" 
+                        onChange={this.handleNicknameChange} />
+            Last Name: <input type="text"
+                        onChange={this.handleLastNameChange} />
+            <br />
+            {/*Date of Birth: <DateDropdown handleChange={this.handleBirthdayChange} />*/}
+            Email: <input type="text"
+                    onChange={this.handleEmailChange} />
+            <br />
+            ID Provided? <input type="radio" name="idProvided" value="true" 
+                                  checked={this.state.providedID === true}
+                                  onChange={this.handleProvidedIDChange} /> Yes
+                          <input type="radio" name="idProvided" value="false" 
+                                  checked={this.state.providedID === false} 
+                                  onChange={this.handleProvidedIDChange} /> No
+            <IdInfo display={this.state.providedID} 
+                    stateID={this.state.stateID}
+                    changeState={this.handleStateIDChange}
+                    changeOtherID={this.handleOtherIDChange} />
+            <br />
+            How did you hear about Spy? {/* Need textbox on other */}
+            <select name="howHear" defaultValue="outreach" onChange={this.handleHowHeard}>
+              <option value="outreach">SPY Outreach</option>
+              <option value="friends">Friends</option>
+              <option value="web">Web</option>
+              <option value="other">Other</option>
+            </select>
+            {/* Not included: Services */}
+            <br /> 
+            <input type="submit" value="Submit" />
+          </form>
       </div>
     );
   }
 });
 
+// This isn't working properly for some reason...
+var DateDropdown = React.createClass({ 
+  getInitialState: function () {
+    var todaysDate = moment(),
+        month = todaysDate.format("MM"),
+        day = todaysDate.format("DD"),
+        year = todaysDate.format("YYYY");
+    return {month: month, day: day, year: year};
+  },
+  handleMonthChange: function (e) {
+    this.setState({month: e.target.value});
+    if (this.state.day > this.state.month.daysInMonth) {
+      this.state.day = 1
+    };
+    var newDate = moment([this.state.year, this.state.month, this.state.day]);
+    this.props.handleChange(newDate);
+  },
+  handleDayChange: function (e) {
+    this.setState({day: e.target.value})
+    var newDate = moment([this.state.year, this.state.month, this.state.day]);
+    this.props.handleChange(newDate);
+  },
+  render: function () { 
+    var monthSelect = [];
+    for (var i = 0; i < 12; i++) {
+      monthSelect.push(
+        <option value={i + 1} key={"month" + i}>{moment().startOf("year").add(1 * i, "months").format("MMMM")}</option>
+      )
+    };
+    var daySelect = [];
+    var daysInMonth = moment(this.state.year + "-" + this.state.month).daysInMonth();
+    for (var i = 1; i <= daysInMonth; i++) {
+      daySelect.push(
+        <option value={i} key={"day" + i}>{i}</option>
+      )
+    }
+    console.log(this.state.month);
+    return (
+      <div>
+        <select defaultValue={this.state.month} onChange={this.handleMonthChange}>
+          {monthSelect}
+        </select>
+        <select defaultValue={this.state.day} onChange={this.handleDayChange}>
+          {daySelect}
+        </select>
+      </div>
+    );
+  }
+})
+
+// Though this doesn't work either
 var DateOfBirthDropdown = React.createClass({
   getInitialState: function () {
     return {birthMonth: "01", birthDate: "1", birthYear: "2016"};
@@ -138,6 +325,72 @@ var DateOfBirthDropdown = React.createClass({
         </select>
       </div>
     );
+  }
+});
+
+var IdInfo = React.createClass({
+  render: function () {
+    var display = (this.props.display) ? "" : "hidden";
+    return (
+      <div className={display}>
+        ID State: 
+        <select name="state" defaultValue={this.props.stateID} onChange={this.props.changeState}>
+          <option value="AK">AK</option>
+          <option value="AL">AL</option>
+          <option value="AR">AR</option>
+          <option value="AZ">AZ</option>
+          <option value="CA">CA</option>
+          <option value="CO">CO</option>
+          <option value="CT">CT</option>
+          <option value="DC">DC</option>
+          <option value="DE">DE</option>
+          <option value="FL">FL</option>
+          <option value="GA">GA</option>
+          <option value="HI">HI</option>
+          <option value="IA">IA</option>
+          <option value="ID">ID</option>
+          <option value="IL">IL</option>
+          <option value="IN">IN</option>
+          <option value="KS">KS</option>
+          <option value="KY">KY</option>
+          <option value="LA">LA</option>
+          <option value="MA">MA</option>
+          <option value="MD">MD</option>
+          <option value="ME">ME</option>
+          <option value="MI">MI</option>
+          <option value="MN">MN</option>
+          <option value="MO">MO</option>
+          <option value="MS">MS</option>
+          <option value="MT">MT</option>
+          <option value="NC">NC</option>
+          <option value="ND">ND</option>
+          <option value="NE">NE</option>
+          <option value="NH">NH</option>
+          <option value="NJ">NJ</option>
+          <option value="NM">NM</option>
+          <option value="NV">NV</option>
+          <option value="NY">NY</option>
+          <option value="OH">OH</option>
+          <option value="OK">OK</option>
+          <option value="OR">OR</option>
+          <option value="PA">PA</option>
+          <option value="RI">RI</option>
+          <option value="SC">SC</option>
+          <option value="SD">SD</option>
+          <option value="TN">TN</option>
+          <option value="TX">TX</option>
+          <option value="UT">UT</option>
+          <option value="VA">VA</option>
+          <option value="VT">VT</option>
+          <option value="WA">WA</option>
+          <option value="WI">WI</option>
+          <option value="WV">WV</option>
+          <option value="WY">WY</option>
+        </select>
+        <br />
+        Other Forms of ID: <input type="text" onChange={this.props.changeOtherID} />
+      </div>
+    )
   }
 });
 
