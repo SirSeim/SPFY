29f799c6f4613d0b4c10a323983f78f9f350aa52
cf7 <cfranco7@lion.lmu.edu>
2016-10-18 15:40:50 -0700

    selecting activities from activities table in enrollment

--- static/js/helpers/enrollment.js
+++ static/js/helpers/enrollment.js
@@ -57,6 +57,81 @@ $(function (event) {
         refreshSelectedClients();
     });
 
+    var selectedActivities = [];
+
+    var addActivities = function (activities) {
+        var names = [];
+        for (var i = 0; i < selectedActivities.length; i++) {
+            names.push(selectedActivities[i]);
+        }
+        console.log("activity names");
+        console.log(names);
+        console.log(activities);
+        for (var i = 0; i < activities.length; i++) {
+            if (names.indexOf(activities[i].activity_name) !== -1) {
+                console.log(activities[i].activity_name);
+                $('#selected-activities').append('<li class="list-group-item activity">' 
+                        + activities[i].activity_name
+                        + '</li>');
+            }
+        }
+    };
+
+    var refreshSelectedActivities = function () {
+
+
+        var populateActivities = function (dropins) {
+            var currentDropIn = dropins.result[dropins.result.length - 1];
+            console.log(currentDropIn.id);
+
+            $.ajax({
+                url: "api/dropins/" + currentDropIn.id + "/activities",
+                method: "GET",
+                success: function (data) {
+                    console.log("refreshSelectedActivities");
+                    console.log(data);
+                    $('#selected-activities').empty();
+                    var activities = [];
+                    for (var i = 0; i < data.result.rows.length; i++) {
+                        var local = data.result.rows[i];
+                        activities.push({
+                            id: local.id,
+                            activity_name: local.activity_name
+                        });
+                    }
+                    addActivities(activities);
+                },
+                error: function (data) {
+                    console.error(data);
+                }
+            });
+        };
+
+        var getDropins = function () {
+            $.ajax({
+                url: "api/dropins",
+                method: "GET",
+                success: function (data) {
+                    populateActivities(data);
+                    console.log("drop-ins");
+                    console.log(data);
+                },
+                error: function (data) {
+                    console.error(data);
+                }
+            });
+        };
+
+        getDropins();
+
+    };
+
+    $('#activities').delegate("td", "click", function () {
+        console.log($(this)[0].innerText);
+        selectedActivities.push($(this)[0].innerText);
+        refreshSelectedActivities();
+    });
+
     // $.ajax({
     //     url: "api/dropins",
     //     method: "GET",
