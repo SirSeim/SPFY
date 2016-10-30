8eca0cab9e5f6cc6d17321bd0ce713f673fa847c
cf7 <cfranco7@lion.lmu.edu>
2016-10-29 21:47:30 -0700

    implementing notify.js jquery plugin

--- static/js/casemanager.js
+++ static/js/casemanager.js
@@ -12,4 +12,27 @@ $(function (event) {
         event.preventDefault();
     });
 
+    // plugin for jquery (adds methods to jquery prototype object)
+    // any jquery object can now use these methods
+    // plugin makes its way out of plugins/notify.js and into the other js files
+    // through _basescript.html script tag 
+    $.notify("Hello SPY!");
+
+    // sets global default attributes
+    $.notify.defaults({ className: "success" });
+
+    // second parameter is className
+    $.notify("Hello SPY!", "success");
+
+    // second parameter can also be an object with
+    // more options
+    $("#clients").notify("Hello SPY!", {
+        className: "info",
+        position: "right"
+    });
+
+
+    /* MDN has a Notification API for desktop notifications
+    would that be useful to the staff?
+    */
 });
