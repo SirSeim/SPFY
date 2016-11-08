005022237aeefdffe1a827e7ac49dfa8427ebc24
cjdellomes <cjdellomes@gmail.com>
2016-11-07 21:09:11 -0800

    Retrieving file as base64, working on previewing file

--- static/js/files.js
+++ static/js/files.js
@@ -1,6 +1,6 @@
 $(function (event) {
 
-	var arrayBufferToBase64 = function (buffer) {
+	var arrayBufferToBase64 = function (buffer, reader) {
 		var base64 = '';
 		var bytes = new Uint8Array(buffer);
 		var len = bytes.byteLength;
@@ -12,17 +12,43 @@ $(function (event) {
 		return base64;
 	}
 
+	//TODO: Get this working
+	// var base64ToFile = function (base64) {
+	// 	var img_b64 = canvas.toDataURL(base64);
+	// 	var png = img_b64.split(',')[1];
+
+	// 	var the_file = new Blob([window.atob(png)],  {type: 'image/png', encoding: 'utf-8'});
+
+	// 	var fr = new FileReader();
+	// 	fr.onload = function ( oFREvent ) {
+	// 	    var v = oFREvent.target.result.split(',')[1]; // encoding is messed up here, so we fix it
+	// 	    v = atob(v);
+	// 	    var good_b64 = btoa(decodeURIComponent(escape(v)));
+	// 	    document.getElementById("uploadPreview").src = "data:image/png;base64," + good_b64;
+	// 	};
+	// 	fr.readAsDataURL(the_file);
+	// }
+
 	$('#file').change(function () {
 		var reader = new FileReader();
+		var file = this.files[0];
+		var base64;
 
 		reader.onload = function() {
 
-			var base64 = arrayBufferToBase64(this.result);
+			var preview = document.querySelector('img[id=preview]');
+			preview.src = reader.result;
+
+			var base64 = arrayBufferToBase64(reader.result);
     		console.log(base64);
     		console.log('DONE');
 		}
 
-		reader.readAsArrayBuffer(this.files[0]);
+		
+		if (file) {
+			reader.readAsArrayBuffer(file);
+		}
+		//reader.readAsDataURL(file);
 	});
 
 });
