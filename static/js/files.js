b46214452ce7cce084596c04e30dc4d28f8836e4
cjdellomes <cjdellomes@gmail.com>
2016-11-07 20:04:12 -0800

    Testing converting file to base64 string

--- static/js/files.js
+++ static/js/files.js
@@ -1,7 +1,28 @@
 $(function (event) {
 
-	var fileReader = new FileReader();
-	fileReader.onLoad = function () {
-	};
+	var arrayBufferToBase64 = function (buffer) {
+		var base64 = '';
+		var bytes = new Uint8Array(buffer);
+		var len = bytes.byteLength;
+
+		for (var i = 0; i < len; i++) {
+    		base64 += String.fromCharCode( bytes[ i ] );
+		}
+
+		return base64;
+	}
+
+	$('#file').change(function () {
+		var reader = new FileReader();
+
+		reader.onload = function() {
+
+			var base64 = arrayBufferToBase64(this.result);
+    		console.log(base64);
+    		console.log('DONE');
+		}
+
+		reader.readAsArrayBuffer(this.files[0]);
+	});
 
 });
