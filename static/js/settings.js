18b680631c6fd77b9be185d82d1dc711c75ae860
cf7 <cfranco7@lion.lmu.edu>
2016-11-08 02:07:30 -0800

    experimenting with initial react and jquery

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,23 +1,70 @@
-
-
-var Settings = React.createClass({
-    render: function () {
-        return (
-            <div className="text">
-                <h4>There is text here</h4>
-            </div>
-        )
-    }
-});
-
-ReactDOM.render(
-  <Settings />,
-  document.getElementById('react-content')
-);
-
 $(function () {
     $('#options').delegate('li', 'click', function (event) {
-        $('#options li').removeClass('selected');
+        $('#options li.selected').removeClass('selected');
         $(this).addClass('selected');
     });
+
+    $('#notifications-link').click(function (event) {
+        $('#notifications-settings').removeClass('hidden');
+        $('#client-profile-settings').addClass('hidden');
+    });
+    $('#client-profile-link').click(function (event) {
+        $('#client-profile-settings').removeClass('hidden');
+        $('#notifications-settings').addClass('hidden');
+    });
 });
+
+// var ListItem = React.createClass({
+//     getInitialState: function () {
+//         return {
+//             isSelected: false
+//         };
+//     },
+//     handleClick: function () {
+//         this.setState({
+//             isSelected: true
+//         });
+//     },
+//     render: function () {
+//         var isSelected = this.state.isSelected;
+//         if (isSelected) {
+//             return (
+//                 <li className="selected" onClick={this.handleClick}><a href="#">{this.props.content}</a></li>
+//                 // getting this from whenever a ListItem is created <ListItem content="Notifications" />
+//             );
+//         } else {
+//             return (
+//                 <li onClick={this.handleClick}><a href="#">{this.props.content}</a></li>
+//                 // getting this from whenever a ListItem is created <ListItem content="Notifications" />
+//             );
+//         }
+//     }
+// })
+
+// var Navs = React.createClass({
+//     handleClick: function () {
+//         $('#options li.selected').removeClass('selected');
+//     },
+//     render: function () {
+//         return (
+            // <div className="row">
+            //   <div id="navigation" className="col-sm-3 col-lg-2">
+//              <nav className="side-nav">
+//                 <ul id="options" onClick={this.handleClick}>
+//                     <ListItem content="One Thing" />
+//                     <ListItem content="Another Thing" />
+//                 </ul>
+//             </nav>
+            //    </div>
+            //    <div class="col-sm-9 col-lg-10">
+            //     <!-- your page content -->
+            //     </div>
+            // </div>
+//         )
+//     }
+// });
+
+// ReactDOM.render(
+//   <Navs />,
+//   document.getElementById('react-content')
+// );
