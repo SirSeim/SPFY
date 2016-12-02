d6f6dbdcea5e37adef10187dda70bd4cbfcb012d
Edward Seim <sirseim@gmail.com>
2016-12-01 17:53:45 -0800

    minor console log for request path

--- api/validate.js
+++ api/validate.js
@@ -1,6 +1,6 @@
 module.exports = function (decoded, request, callback) {
     console.log(decoded);
-    // console.log(request);
+    console.log(request.path);
     // TODO: Look into what is in decoded & request
     // query the database for the user
     return callback(null, true);
