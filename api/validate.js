cdcff61ac4323688c71ebaa77a0edecd516f680d
Edward Seim <sirseim@gmail.com>
2016-12-10 13:00:32 -0800

    Looking at path fingerprints #46

--- api/validate.js
+++ api/validate.js
@@ -1,6 +1,6 @@
 module.exports = function (decoded, request, callback) {
     console.log(decoded);
-    console.log(request.path);
+    console.log(request.route.fingerprint);
     // TODO: Look into what is in decoded & request
     // query the database for the user
     return callback(null, true);
