aa2e3397fd68aae387b26cf43df3344d4e3568d3
Chris Dellomes <cjdellomes@gmail.com>
2017-01-21 19:26:22 -0800

    Working on add, edit, and delete of followups

--- static/js/profile.js
+++ static/js/profile.js
@@ -6,7 +6,6 @@ $(function (event) {
     console.log('TEST');
 
 	var getCaseManagerFollowUps = function (casemanagerID) {
-        console.log('getCaseManagerFollowUps called');
 		$.ajax({
             xhrFields: {
                 withCredentials: true
@@ -17,7 +16,6 @@ $(function (event) {
             url: 'api/followups/' + casemanagerID,
             method: 'GET',
             success: function (data) {
-                console.log('SUCCESS');
                 console.log(data);
                 tableBody.empty();
 
@@ -48,6 +46,86 @@ $(function (event) {
         });
 	};
 
-    getCaseManagerFollowUps(localStorage.userID);
+    var createFollowUp = function (data) {
+        $.ajax({
+            xhrFields: {
+                withCredentials: true
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', localStorage.getItem("authorization"));
+            },
+            url: 'api/followups/',
+            method: 'POST',
+            data: data,
+            success: function (data) {
+                console.log(data);
+                getCaseManagerFollowUps(localStorage.getItem("userID"));
+            },
+            error: function (xhr) {
+                console.log(xhr);
+
+                if (xhr.status === 401) {
+                    localStorage.removeItem("authorization");
+                }
+            }
+        }).done(function (data) {
+
+        });
+    };
+
+    var editFollowUp = function (data) {
+        $.ajax({
+            xhrFields: {
+                withCredentials: true
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', localStorage.getItem("authorization"));
+            },
+            url: 'api/followups/' + data.id,
+            method: 'POST',
+            data: data,
+            success: function (data) {
+                console.log(data);
+                getCaseManagerFollowUps(localStorage.getItem("userID"));
+            },
+            error: function (xhr) {
+                console.log(xhr);
+
+                if (xhr.status === 401) {
+                    localStorage.removeItem("authorization");
+                }
+            }
+        }).done(function (data) {
+
+        });
+    };
+
+    var deleteFollowUp = function (id) {
+        $.ajax({
+            xhrFields: {
+                withCredentials: true
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', localStorage.getItem("authorization"));
+            },
+            url: 'api/followups/' + id,
+            method: 'POST',
+            success: function (data) {
+                console.log(data);
+                getCaseManagerFollowUps(localStorage.getItem("userID"));
+            },
+            error: function (xhr) {
+                console.log(xhr);
+
+                if (xhr.status === 401) {
+                    localStorage.removeItem("authorization");
+                }
+            }
+        }).done(function (data) {
+
+        });
+    };
+
+    getCaseManagerFollowUps(localStorage.getItem("userID"));
 
 });
